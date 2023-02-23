d6cdbc920ebb4355f9a9384abfd90532fa222a80
dingdot <2659384812@qq.com>
2023-02-23 17:59:25 +0800

    update gitignore

--- genshinwebsite/genshinwebsite/bin/Release/netcoreapp3.1/publish/wwwroot/js/login.js
+++
@@ -1,9 +0,0 @@
-﻿function submit_click() {
-    if ($("#accountInput").val() == "" || $("#passwordInput").val() == "") {
-        console.log("应当填写完整信息");
-        alert("应当填写完整信息");
-    }
-    else {
-        $("form").submit();
-    }
-}
